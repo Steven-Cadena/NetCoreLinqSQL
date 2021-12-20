5919cff303f41753797d8b8bf71a2f54632ba5c7
Steven-Cadena <steven.cadena.giler@gmail.com>
2021-12-20 13:20:50 +0100

    Add project files.

---
+++ NetCoreLinqSQL/Controllers/EmpleadosController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreLinqSQL.Models;
+using NetCoreLinqSQL.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreLinqSQL.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        EmpleadosContext context;
+
+        //constructor
+        public EmpleadosController() 
+        {
+            this.context = new EmpleadosContext();
+        }
+
+        public IActionResult TodosEmpleados() 
+        {
+            List<Empleado> empleados = this.context.GetEmpleados();
+            return View(empleados);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int idempleado) 
+        {
+            Empleado empleado = this.context.FindEmpleado(idempleado);
+            return View(empleado);
+        }
+    }
+}
